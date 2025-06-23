728163c562d9414fcf597e9b31a7f261d6c26f40
y4zper <ander.crr@gmail.com>
2025-06-23 12:33:42 -0500

    fin2

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -7,8 +7,7 @@ const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
-router.post("/logout",logout);
-router.post("/refresh-token",refreshToken);
-router.get("/profile",protectRoute, getProfile);
-
+router.post("/logout", logout);
+router.post("/refresh-token", refreshToken);
+router.get("/profile", protectRoute, getProfile);
 export default router;
--- backend/routes/cart.route.js
+++ backend/routes/cart.route.js
@@ -15,4 +15,5 @@ router.post("/", protectRoute, addToCart);
 router.delete("/", protectRoute, removeAllFromCart);
 router.put("/:id", protectRoute, updateQuantity);
 
+
 export default router;
