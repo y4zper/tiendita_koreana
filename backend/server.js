491e967f1bc9383586755fec9cf75381af4c364c
y4zper <ander.crr@gmail.com>
2025-06-23 19:02:33 -0500

    finnns

--- backend/server.js
+++ backend/server.js
@@ -33,12 +33,11 @@ app.use("/api/analytics", analyticsRoutes);
 
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-  // Solo manejar rutas que **no empiecen con /api/**
-  app.get(/^\/(?!).*/, (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-  });
+	app.get(/^\/(?!api).*/, (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
 }
 
 
